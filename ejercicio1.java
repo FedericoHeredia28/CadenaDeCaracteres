b9676c3a821efab50e1bbc818cd80ccb46787a60
FedericoHeredia28 <102254038+FedericoHeredia28@users.noreply.github.com>
2023-08-28 14:46:48 -0300

    Add files via upload

---
+++ ejercicio1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ingrese su nombre: ");
+		String nombre = scanner.nextLine();
+		System.out.printf("%s", nombre);
+	}
+
+}
