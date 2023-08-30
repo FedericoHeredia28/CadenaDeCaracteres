ca7031f163c279174be192e40017517934740aa2
FedericoHeredia28 <102254038+FedericoHeredia28@users.noreply.github.com>
2023-08-30 08:58:11 -0300

    Add files via upload

---
+++ ejer5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ejer5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String linea = scanner.nextLine();
+		String[] array = linea.split(" ");
+		String elemento1 = array[0];
+		String elemento2 = array[1];
+		if(elemento1.length() != elemento2.length() ) {
+			System.out.println("No son anagramas");
+			return;
+		}
+		int charCheck = 0;
+		int esAnagrama = elemento1.length();
+		for(int i = 0; i < elemento1.length(); i++) { 
+			for(int j = 0; j < elemento2.length(); j++) {
+				if(elemento1.charAt(i) == elemento2.charAt(j)) charCheck = 1;
+			}
+			if(charCheck == 1) esAnagrama--;
+			charCheck = 0;
+			if (esAnagrama == 0) {
+				System.out.println("Son anagramas");
+				return;
+			}
+		}
+		System.out.println("No son anagramas");
+		
+		scanner.close();
+	}
+
+}
